3659f81ba0d451ef72f800a7cdfcef838da8da2c
Daniele Mannino <danymannino@gmail.com>
2023-06-23 19:56:47 +0200

    Modifiche a fragment profilo

--- app/src/main/java/com/example/autenticazione_v1/ProfileFragment.java
+++ app/src/main/java/com/example/autenticazione_v1/ProfileFragment.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +29,12 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    ImageView profilePic;
+    FirebaseUser user;
+    FirebaseAuth auth;
+    TextView t;
+
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -53,12 +65,27 @@ public class ProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();  //prende il current user
+        //System.out.println(user.getEmail());
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        profilePic = getActivity().findViewById(R.id.profilePict);
+        t = view.findViewById(R.id.casellaNome);
+        t.setText("Nome : " + user.getEmail());
+
+
+        //profilePic.setImageBitmap();
+
+        //return inflater.inflate(R.layout.fragment_profile, container, false);
+        return view;
     }
 }
